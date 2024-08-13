be789751adcfedf79d8239b1d892927fb821dda5
NF4Caniago <afifilhamcaniago@gmail.com>
2024-08-13 21:44:51 +0700

    27:32

--- errors.go
+++ errors.go
@@ -1,5 +1,35 @@
 package main
+import (
+	"fmt"
+	"errors"
+)
+
+var (
+	ValidationError = errors.New("Validation Error")
+	NotFoundError = errors.New("Not Found Error") 
+)
+
+func GetById(id string) error {
+	if id == "" {
+		return ValidationError
+	} 
+	
+	if id != "afif" {
+		return NotFoundError
+	}
+
+	return nil
+}
 
 func main() {
-
+	err := GetById("eko")
+	if err != nil {
+		if errors.Is(err, ValidationError) {
+			fmt.Println("validation error")
+		} else if errors.Is(err, NotFoundError) {
+			fmt.Println("not found error")
+		} else {
+			fmt.Println("unknown error")
+		}
+	}
 }
