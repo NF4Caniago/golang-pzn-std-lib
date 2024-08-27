ef5ff259637a859c0d9feb86e9b195815513111e
NF4Caniago <afifilhamcaniago@gmail.com>
2024-08-27 07:59:32 +0700

    1:36:22

--- reflect.go
+++ reflect.go
@@ -9,23 +9,47 @@ type Sample struct {
 }
 
 type Person struct {
-	Name string
-	Address string
-	Email string
+	Name string `required:"true" max:"10"`
+	Address string `required:"true" max:"10"`
+	Email string `required:"true" max:"10"`
 }
 
 func readField(value any) {
 	var valueType reflect.Type = reflect.TypeOf(value)
 	fmt.Println("Type Name", valueType.Name())
 	for i := 0; i < valueType.NumField(); i++ {
-		var valueField reflect.StructField = valueType.Field(i)
-		fmt.Println(valueField.Name , "with type", valueField.Type)
-		fmt.Println(StructField.Tag.Get("required"))
-		fmt.Println(StructField.Tag.Get("max"))
+		var structField reflect.StructField = valueType.Field(i)
+		fmt.Println(structField.Name , "with type", structField.Type)
+		fmt.Println(structField.Tag.Get("required"))
+		fmt.Println(structField.Tag.Get("max"))
 	}
+}
+
+func isValid(value any) (result bool) {
+	result = true
+	var t reflect.Type = reflect.TypeOf(value)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if f.Tag.Get("required") == "true" {
+			data := reflect.ValueOf(value).Field(i).Interface()
+			result = data != ""
+			if result == false {
+				return result
+			}
+		}
+	}
+	return result
 }
 
 func main() {
 	readField(Sample{"Eko"})
 	readField(Person{"Eko", "", ""})
+
+	person := Person{
+		Name : "afif",
+		Address : "",
+		Email : "asd",
+	}
+
+	fmt.Println(isValid(person))
 }
